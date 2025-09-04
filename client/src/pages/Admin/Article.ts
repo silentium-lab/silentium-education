37514d11e79e0e55deac70b47569a72d87324463
Eugene Kosukhin <kosuha606@gmail.com>
2025-09-04 23:27:07 +0300

    admin

--- client/src/pages/Admin/Article.ts
+++ client/src/pages/Admin/Article.ts
@@ -1,6 +1,7 @@
 import { type OwnerType, TheInformation } from "silentium";
 import { Template } from "silentium-components";
 import { i18n, titleSrc } from "../../store";
+import { Link } from "../../components/Link";
 
 export class Article extends TheInformation {
 	value(o: OwnerType<unknown>): this {
@@ -8,6 +9,7 @@ export class Article extends TheInformation {
 
 		const t = new Template();
 		t.template(`<div class="article">
+			${t.var(new Link("/admin/articles", i18n.tr("Articles"), "underline"))}
         <h1 class="title-1">${t.var(title)}</h1>
       </div>`).value(o);
 
--- client/src/pages/Admin/Articles.ts
+++ client/src/pages/Admin/Articles.ts
@@ -1,6 +1,7 @@
 import { type OwnerType, TheInformation } from "silentium";
 import { Template } from "silentium-components";
 import { i18n, titleSrc } from "../../store";
+import { Link } from "../../components/Link";
 
 export class Articles extends TheInformation {
 	value(o: OwnerType<unknown>): this {
@@ -9,6 +10,15 @@ export class Articles extends TheInformation {
 		const t = new Template();
 		t.template(`<div class="article">
         <h1 class="title-1">${t.var(title)}</h1>
+        <div>
+          ${t.var(new Link("/admin/articles/1", i18n.tr("Article 1"), "underline"))}
+        </div>
+        <div>
+          ${t.var(new Link("/admin/articles/2", i18n.tr("Article 2"), "underline"))}
+        </div>
+        <div>
+          ${t.var(new Link("/admin/articles/3", i18n.tr("Article 3"), "underline"))}
+        </div>
       </div>`).value(o);
 
 		return this;
