c5818d031583fb27694a3795f2b633bd4710cf17
Eugene Kosukhin <kosuha606@gmail.com>
2025-09-20 17:25:30 +0300

    notification after remove

--- client/src/pages/Admin/Articles.ts
+++ client/src/pages/Admin/Articles.ts
@@ -1,6 +1,6 @@
 import { Any, Applied, Chain, From, Late, Lazy, Map, Of, Once, type OwnerType, Shared, SharedSource, TheInformation } from "silentium";
 import { Const, Path, RecordOf, Shot, Template } from "silentium-components";
-import { backendCrudSrc } from "../../bootstrap";
+import { backendCrudSrc, notificationSrc } from "../../bootstrap";
 import { Mustache } from "../../modules/plugins/mustache/Mustache";
 import { i18n, titleSrc } from "../../store";
 import { ClickedId } from "../../modules/ClickedId";
@@ -19,26 +19,32 @@ export class Articles extends TheInformation {
           Создать статью
         </a>
         ${t.var(new Applied(
-          new Any<any>(new Chain(articlesSearchSrc, new Of([])), new Map(articlesSrc, new Lazy(
-            (article) => {
-              const removeTrigger = new Shared(new Late());
-              const removedSrc = backendCrudSrc.ofModelName('articles').deleted(
-                new Shot(new Path(article, new Of('_id')), removeTrigger)
-              );
-              new Const({}, new Once(removedSrc)).value(articlesSearchSrc);
-              return new Mustache(`<div class="flex gap-2">
+      new Any<any>(new Chain(articlesSearchSrc, new Of([])), new Map(articlesSrc, new Lazy(
+        (article) => {
+          const removeTrigger = new Shared(new Late());
+          const removedSrc = backendCrudSrc.ofModelName('articles').deleted(
+            new Shot(new Path(article, new Of('_id')), removeTrigger)
+          );
+          new Const({}, new Once(removedSrc)).value(articlesSearchSrc);
+
+          new Const({
+            type: 'success',
+            content: 'Успешно удалено'
+          }, removedSrc).value(notificationSrc);
+
+          return new Mustache(`<div class="flex gap-2">
                 <a href="/admin/articles/{{ article._id }}/" class="underline">
                   {{ article.title }}
                 </a>
                 <div class="cursor-pointer {{ removeId }}">&times;</div>
               </div>`, new RecordOf({
-                article,
-                removeId: new ClickedId(removeTrigger),
-              })).addDep(removeTrigger).addDep(removedSrc)
-            }
-          ))),
-          (a) => a.join('')
-        ))}
+            article,
+            removeId: new ClickedId(removeTrigger),
+          })).addDep(removeTrigger).addDep(removedSrc)
+        }
+      ))),
+      (a) => a.join('')
+    ))}
       </div>`).value(o);
 
     return this;
