0ca71b6993312e3e6c6400f31247834473ef235f
Eugene Kosukhin <kosuha606@gmail.com>
2025-10-05 10:49:43 +0300

    link fixed

--- client/src/components/Link.ts
+++ client/src/components/Link.ts
@@ -3,26 +3,27 @@ import {
 	of,
 	on,
 	primitive,
-	shared
+	shared,
 } from "silentium";
 import { first, template } from "silentium-components";
 import { elements } from "silentium-web-api";
 import { className } from "../modules/ClassName";
 import { clicked } from "../modules/Clicked";
 import { id } from "../modules/Id";
+import { urlSrc } from "../store";
 
 export const link = (
-	urlSrc: DataType<string>,
+	linkUrlSrc: DataType<string>,
 	textSrc: DataType<string>,
 	classSrc: DataType<string> = of(""),
 ): DataType<string> => (user) => {
 	const idSrc = shared(id());
-	const sharedUrlSrc = shared(urlSrc);
-	const urlSync = primitive(urlSrc);
+	const sharedUrlSrc = shared(linkUrlSrc);
+	const urlSync = primitive(linkUrlSrc);
 
 	on(clicked(first(elements(className(idSrc.value)))), (e) => {
 		e.preventDefault();
-		sharedUrlSrc.give(urlSync.primitive() as string);
+		urlSrc.give(urlSync.primitive() as string);
 	});
 
 	const t = template();
