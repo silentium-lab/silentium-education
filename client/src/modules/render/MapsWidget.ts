eb34ed25a7e1f9bd59b2b5b401669454519ad9cb
Eugene Kosukhin <kosuha606@gmail.com>
2026-01-01 17:37:26 +0300

    fix(client): maps widget

---
+++ client/src/modules/render/MapsWidget.ts
@@ -0,0 +1,18 @@
+import { html } from "@/modules/plugins/lang/html";
+import { ActualMessage, Applied, MaybeMessage } from "silentium";
+
+/**
+ * Maps widget searches for [map?url=...]
+ */
+export function MapsWidget(_base: MaybeMessage<string>) {
+  const $base = ActualMessage(_base);
+  return Applied($base, (base) => {
+    return base.replace(/\[map\?url=(.*?)\]/gs, (_, url) => {
+      return html`
+        <div class="articles-in-article">
+          <iframe src="${url}" border="0" height="500" width="100%" />
+        </div>
+      `;
+    });
+  });
+}
--- client/src/modules/render/Widgets.ts
+++ client/src/modules/render/Widgets.ts
@@ -1,10 +1,11 @@
 import { ArticleListWidget } from "@/modules/render/ArticleListWidget";
 import { BlogWidget } from "@/modules/render/BlogWidget";
+import { MapsWidget } from "@/modules/render/MapsWidget";
 import { MessageType, Piped } from "silentium";
 
 /**
  * Process widgets on text
  */
 export function Widgets($base: MessageType<string>) {
-  return Piped($base, ArticleListWidget, BlogWidget);
+  return Piped($base, ArticleListWidget, BlogWidget, MapsWidget);
 }
